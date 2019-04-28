6b02eb362fd1ed1e42c837981f245817eb80fb8b
DacheOA <dachely.otero.argote@gmail.com>
2019-04-28 07:30:37 -0500

    merging branch

--- routes/root.js
+++ routes/root.js
@@ -37,6 +37,8 @@ router.post("/guardar_text", (req, res) => {
   console.log(req.body.text);
 });
 
+let data = [];
+
 async function getTweets() {
   var db = cloudant.db.use("tweetdb");
 
@@ -45,8 +47,22 @@ async function getTweets() {
       console.log("Fecha no encontrada en la base de datos: ", err);
       throw err;
     }
-    console.log(result.docs[0].tweets);
+    data = result.docs[0].tweets;
+    //console.log(result.docs[0].tweets);
+    console.log(data);
   });
 }
+
+/*async function getTweets() {
+    var db = cloudant.db.use("tweetdb");
+  
+    db.find({ selector: { _id: "2500" } }, function(err, result) {
+      if (err) {
+        console.log("Fecha no encontrada en la base de datos: ", err);
+        throw err;
+      }
+      console.log(result.docs[0].tweets);
+    });
+  }*/
 
 module.exports = router;
